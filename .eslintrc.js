a7cb9d88d29449e5e2f504c3ceead5c934813786
Rajkumar Murugesan <101170753+rajkumarm95@users.noreply.github.com>
2024-07-01 02:22:34 +0530

    fix: enahance ments on quality

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'security'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended'
